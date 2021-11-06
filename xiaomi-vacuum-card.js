c853173df49312a062bb82903e65e8b08b6a7b4f
czesiu20 <czesiu20@tlen.pl>
2021-11-06 23:11:38 +0100

    fix ha-icon-button issue

--- xiaomi-vacuum-card.js
+++ xiaomi-vacuum-card.js
@@ -306,9 +306,8 @@
             return data && data.show !== false
                 ? html`<ha-icon-button
                     @click="${() => this.callService(data.service, data.service_data)}"
-                    icon="${data.icon}"
                     title="${data.label || ''}"
-                    style="${this.config.styles.icon}"></ha-icon-button>`
+                    style="${this.config.styles.icon}"><ha-icon style="display:flex;" icon="${data.icon}"></ha-icon></ha-icon-button>`
                 : null;
         }
 
